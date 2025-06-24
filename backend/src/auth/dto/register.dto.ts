94b16b200b4bf989f9cefdb97321436a9b32104c
MarkOtwane <otwanemark254@gmail.com>
2025-06-24 12:40:54 +0300

    backend and frontend

--- backend/src/auth/dto/register.dto.ts
+++ backend/src/auth/dto/register.dto.ts
@@ -1,17 +1,11 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsOptional, IsString, MinLength } from "class-validator";
 
 export class RegisterDto {
-  @IsEmail()
-  email: string;
+	@IsOptional()
+	@IsEmail()
+	email: string;
 
-  @IsString()
-  @MinLength(8)
-  // @Matches(
-  //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-  //   {
-  //     message:
-  //       'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
-  //   },
-  // )
-  password: string;
+	@IsString()
+	@MinLength(8)
+	password: string;
 }
